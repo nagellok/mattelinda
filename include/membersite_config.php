34152ff8e47269d04c967b561c60312fe3f1b025
nagellok <noac.navertun@gmail.com>
2018-01-31 22:50:38 +0100

    Update membersite_config.php
    changed back to localhost

--- include/membersite_config.php
+++ include/membersite_config.php
@@ -4,7 +4,7 @@ require_once("./include/fg_membersite.php");
 $fgmembersite = new FGMembersite();
 
 //Provide your site name here
-$fgmembersite->SetWebsiteName('mattelinda.se');
+$fgmembersite->SetWebsiteName('www.mattelinda.se');
 
 //Provide the email address where you want to get notifications
 $fgmembersite->SetAdminEmail('info@mattelinda');
@@ -13,7 +13,7 @@ $fgmembersite->SetAdminEmail('info@mattelinda');
 //hostname, user name, password, database name and table name
 //note that the script will create the table (for example, fgusers in this case)
 //by itself on submitting register.php for the first time
-$fgmembersite->InitDB(/*hostname*/'www.mattelinda.se',
+$fgmembersite->InitDB(/*hostname*/'localhost',
                       /*username*/'noac',
                       /*password*/'!QA2ws3ed',
                       /*database name*/'userdb',
